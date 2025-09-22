1e18d0df85f520099721db167001e81e339a8e47
aelf0x800 <199596498+aelf0x800@users.noreply.github.com>
2025-09-22 22:43:55 +0100

    Start improving error handling

--- Engine/Engine.cpp
+++ Engine/Engine.cpp
@@ -5,6 +5,7 @@
 #pragma comment(lib, "d3d11.lib")
 #pragma comment(lib, "D3DCompiler.lib")
 
+#define TRY(X) if (FAILED(X)) throw DirectXException();
 #define RELEASE(X) if (X != nullptr) X->Release()
 
 namespace Engine
@@ -17,32 +18,37 @@ Engine::Engine() : Window(L"Engine", 1600, 900),
                    m_target(nullptr),
                    m_isOpen(false)
 {
-    DXGI_SWAP_CHAIN_DESC swapChainDesc = {};
-    swapChainDesc.BufferDesc.Width = 0;
-    swapChainDesc.BufferDesc.Height = 0;
-    swapChainDesc.BufferDesc.Format = DXGI_FORMAT_B8G8R8A8_UNORM;
-    swapChainDesc.BufferDesc.RefreshRate.Numerator = 0;
-    swapChainDesc.BufferDesc.RefreshRate.Denominator = 0;
-    swapChainDesc.BufferDesc.Scaling = DXGI_MODE_SCALING_UNSPECIFIED;
-    swapChainDesc.BufferDesc.ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED;
-    swapChainDesc.SampleDesc.Count = 1;
-    swapChainDesc.SampleDesc.Quality = 0;
-    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    swapChainDesc.BufferCount = 1;
-    swapChainDesc.OutputWindow = m_hWnd;
-    swapChainDesc.Windowed = TRUE;
-    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
-    swapChainDesc.Flags = 0;
+    DXGI_SWAP_CHAIN_DESC swapChainDesc =
+    {
+        .BufferDesc =
+        {
+            .Width = 0,
+            .Height = 0,
+            .RefreshRate =
+            {
+                .Numerator = 0,
+                .Denominator = 0
+            },
+            .Format = DXGI_FORMAT_B8G8R8A8_UNORM,
+            .ScanlineOrdering = DXGI_MODE_SCANLINE_ORDER_UNSPECIFIED,
+            .Scaling = DXGI_MODE_SCALING_UNSPECIFIED
+        },
+        .SampleDesc =
+        {
+            .Count = 1,
+            .Quality = 0
+        },
+        .BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT,
+        .BufferCount = 1,
+        .OutputWindow = m_hWnd,
+        .Windowed = TRUE,
+        .SwapEffect = DXGI_SWAP_EFFECT_DISCARD,
+        .Flags = 0
+    };
 
-    D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0,
+    TRY(D3D11CreateDeviceAndSwapChain(nullptr, D3D_DRIVER_TYPE_HARDWARE, nullptr, 0, nullptr, 0,
                                   D3D11_SDK_VERSION, &swapChainDesc, &m_swapChain, &m_device,
-                                  nullptr, &m_context);
-
-    ID3D11Resource* backBuffer = nullptr;
-    m_swapChain->GetBuffer(0, __uuidof(ID3D11Resource), reinterpret_cast<void**>(&backBuffer));
-    m_device->CreateRenderTargetView(backBuffer, nullptr, &m_target);
-    RELEASE(backBuffer);
-
+                                  nullptr, &m_context));
     m_isOpen = true;
 }
 
